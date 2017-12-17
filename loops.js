49f9091f4b11c10e79d30feec39d36d0468f6c72
William Spangler <bspangs@hotmail.com>
2017-12-17 01:18:25 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,30 @@
+//i is set equal to 0
+//as long as i is less than 25, exexute the code in the loopBody, giving total of 25 executions
+// - which prints  "I am ${1} strange loops if i=1" and "I am ${i} strange loops." for any other value of i
+function forLoop(array){
+  for (i=0; i < 25; i++ ){
+  if (i === 1){ 
+  array.push("I am 1 strange loop");
+    }else {array.push(`I am ${i} strange loops.`);
+          }
+}
+return array
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+ 
+do {
+  console.log('doo-bee-doo-bee-doo')
+} while (maybeTrue());
+
+
+function whileLoop(n) {
+ while (n > 0){
+ console.log(--n)
+ }
+ return "done"
+}
+
+function doWhileLoop(){}
